ea3a97aebb4165e7eabdd9174f8a2fb34d04aab3
qinxuanwu <917595913@qq.com>
2017-11-15 17:40:46 +0800

    init

---
+++ generators/theming/default/adminlte/assets/ThemingAsset.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * This is the template for generating a asset class within a theming.
+ */
+
+/* @var $this yii\web\AssetBundle */
+/* @var $generator yii\gii\generators\theming\Generator */
+
+$namespace = $generator->ns . '\\' . $generator->getThemingName($generator->themingID) . '\assets';
+$adminLte = '\\' . $namespace . '\common\AdminLte';
+$html5shiv = '\\' . $namespace . '\common\Html5shiv';
+$basePathAlias = '@' . str_replace('\\', '/', $generator->ns) . '/' . $generator->getThemingName($generator->themingID) . '/assets';
+
+echo "<?php\n";
+?>
+
+namespace <?= $namespace ?>;
+
+use yii\web\AssetBundle;
+
+/**
+ * Main asset for the `<?= $generator->getThemingName($generator->themingID) ?>` theming
+ */
+
+class ThemingAsset extends AssetBundle
+{
+    public $sourcePath = '<?= $basePathAlias ?>';
+    //public $baseUrl = '@web';
+    public $css = [
+        'css/custom.css',
+    ];
+
+    public $js = [
+        'js/custom.js',
+    ];
+
+    public $depends = [
+        'yii\web\YiiAsset',
+        '<?=$adminLte?>',
+        '<?=$html5shiv?>'
+    ];
+}
